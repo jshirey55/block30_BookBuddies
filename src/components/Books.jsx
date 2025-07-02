8fbb962bc2e9577797aab81889474ba314fa2758
robC_vW <robiecalong2@gmail.com>
2025-07-02 11:23:02 -0500

    testing books update

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,1 +1,11 @@
 /* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
+
+function Books() {
+  return (
+    <div>
+      <h1>hi hi</h1>
+    </div>
+  );
+}
+
+export default Books;
