6d288112a5891902b509fbf7b492af6f13c84d85
robC_vW <robiecalong2@gmail.com>
2025-07-02 19:17:32 -0500

    update books App main and added styles css

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,1 +1,23 @@
+import { Link } from "react-router-dom";
+import bookLogo from "../assets/books.png";
+import "../styles/navigation.css";
+
 /* TODO - add your code to create a functional React component that renders a navigation bar for the different views in your single page application. You may consider conditionally rendering some options - for example 'Login' should be available if someone has not logged in yet. */
+function Navigation() {
+  return (
+    <div id="navbar">
+      <div className="logo-name">
+        <img id="logo-image" src={bookLogo} />
+        <span>BookBuddies</span>
+      </div>
+      <Link className="links" to="/books">
+        Books
+      </Link>
+      <Link className="links" to="/account">
+        Account
+      </Link>
+    </div>
+  );
+}
+
+export default Navigation;
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,1 +1,11 @@
 /* TODO - add your code to create a functional React component that renders details for a single book. Fetch the book data from the provided API. You may consider conditionally rendering a 'Checkout' button for logged in users. */
+import { useParams } from "react-router-dom";
+
+function SingleBook() {
+  //you can useParams to get the SingleBook and fetch the specific book from API based on id
+  const { id } = useParams();
+
+  return <div>the book id is {id}</div>;
+}
+
+export default SingleBook;
